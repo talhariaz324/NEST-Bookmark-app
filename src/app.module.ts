45e5c4c658ab2c1f324414efe92fe435f6b319ee
talhariaz324 <talhariaz324@gmail.com>
2023-09-17 03:13:10 +0500

    Completed without e2e testing,, you can go in the video instructor repo for e2e

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,18 @@ import { BookmarkModule } from './bookmark/bookmark.module';
 import { UserModule } from './user/user.module';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, BookmarkModule, UserModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AuthModule,
+    BookmarkModule,
+    UserModule,
+    PrismaModule,
+  ],
   providers: [PrismaService],
 })
 export class AppModule {}
---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PrismaService } from '../../prisma/prisma.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(
+    config: ConfigService,
+    private prisma: PrismaService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), // It get the token from the headers with by skipping bearer. header: Bearer token
+      secretOrKey: config.get('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: { sub: number; email: string }) {
+    // This validate auto attach the user to req which is made by the user for sign in so that we can return the payload
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: payload.sub,
+      },
+    });
+    delete user.hash;
+    return user;
+  }
+}
